c00d1994c6b9d4f5936aedfdecb90595aedb4a2e
Scott Petit <petit.scott@gmail.com>
2021-01-14 08:09:03 -0600

    Removes problematic nanosecond string from logging

--- Sources/Timber/MessageFormatter.swift
+++ Sources/Timber/MessageFormatter.swift
@@ -22,14 +22,13 @@ public struct MessageFormatter: MessageFormatterType {
     
     public func formatLogMessage(_ logMessage: LogMessage) -> String {
         let components = Calendar.autoupdatingCurrent.dateComponents(calendarComponents, from: logMessage.timestamp)
-        
-        let nanosecondString = "\(components.nanosecond ?? 0)"
-        let timestampString = NSString(format: "%04ld-%02ld-%02ld %02ld:%02ld:%02ld.%@", components.year!,
+
+        let timestampString = NSString(format: "%04ld-%02ld-%02ld %02ld:%02ld:%02ld", components.year!,
             components.month ?? 0,
             components.day ?? 0,
             components.hour ?? 0,
             components.minute ?? 0,
-            components.second ?? 0,        String(nanosecondString[nanosecondString.startIndex..<nanosecondString.index(nanosecondString.startIndex, offsetBy: 3)]))
+            components.second ?? 0)
         let messageToLog = "\(logMessage.logLevel.toString()) \(timestampString) \(appName) [\(logMessage.file) '\(logMessage.function)'] \(logMessage.message)"
         
         return messageToLog
